57a15792e45d76a94503c9184560da7ba3ca6ec5
Vitalik <v.marsenuks@gmail.com>
2024-03-22 17:35:39 +0200

    Btn @ footer page +1 step @ test page added

--- src/main/java/pageobject/InternetShopPagesTest.java
+++ src/main/java/pageobject/InternetShopPagesTest.java
@@ -2,6 +2,7 @@ package pageobject;
 
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
+import pageobject.pages.FooterPage;
 import pageobject.pages.HomePage;
 import pageobject.pages.HoodiesCatalogPage;
 import pageobject.pages.KidsPage;
@@ -13,12 +14,14 @@ public class InternetShopPagesTest {
         baseFunc.openURL("sportsdirect.com");
         HomePage homePage = new HomePage(baseFunc);
         homePage.acceptCookies();
-        homePage.kidsPageOpen();
-
-        KidsPage kidsPage = new KidsPage(baseFunc);
-        kidsPage.hoodiesCatalogOpen();
-
-        HoodiesCatalogPage hoodiesCatalogPage = new HoodiesCatalogPage(baseFunc);
-        hoodiesCatalogPage.setPriceFilter(25,35);
+        FooterPage footerPage = new FooterPage(baseFunc);
+        footerPage.sportsPageOpen();
+        //homePage.kidsPageOpen();
+//
+        //KidsPage kidsPage = new KidsPage(baseFunc);
+        //kidsPage.hoodiesCatalogOpen();
+//
+        //HoodiesCatalogPage hoodiesCatalogPage = new HoodiesCatalogPage(baseFunc);
+        //hoodiesCatalogPage.setPriceFilter(25,35);
     }
 }
--- src/main/java/pageobject/pages/FooterPage.java
+++ src/main/java/pageobject/pages/FooterPage.java
@@ -1,8 +1,15 @@
 package pageobject.pages;
 
 import org.openqa.selenium.By;
+import pageobject.BaseFunc;
 
 public class FooterPage {
     private final By KIDS_MENU = By.xpath(".//a[@href = '/kids']");
     private final By SALE_BTN = By.xpath(".//a[@data-id = '3956267'");
+    private final By SPORTS_BTN = By.id("lnkTopLevelMenu_4091197");
+    private BaseFunc baseFunc;
+
+    public FooterPage(BaseFunc baseFunc) {this.baseFunc = baseFunc;}
+
+    public void sportsPageOpen() {baseFunc.click(SPORTS_BTN);}
 }
